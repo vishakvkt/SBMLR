eab2905d2d5d4260455ea6ef73bdccc74443a4e8
Vishak Venkateswaran <vishak_vkt007@yahoo.com>
2011-07-25 14:09:46 +0100

    Fixed small mistake which threw 'NA' when calculating sensitivity.

--- R/load_simulate_sensitivity.r
+++ R/load_simulate_sensitivity.r
@@ -1,20 +1,30 @@
 "load_simulate_sensitivity" <- function(path_to_sobol, model_path, time_series) {
 
-	simulated_results <-list()
-	sets <- sobolset(path_to_sobol)					#Create the sobol set matrix
-	print('Sobol matrix created successfully')
 	model <- readSBML(model_path)					#Create the model object
 	print('model parsed successfully')
+
+	simulated_results <-list()
+
+	if(path_to_sobol == 1) { 
+	
+		simulated_results <- simulate(model, time_series)
+	}
+
+	else {
+	sets <- sobolset(path_to_sobol)					#Create the sobol set matrix
+	print('Sobol matrix created successfully')
+	
 		
 	simulated_results <- sobol_sim(model, sets, time_series)	#Simulate the model for n sets
 	print('Simulation Completed successfully')
 
 	species_area <- area_matrix(model, simulated_results)		#Use simulation results and calculate area under the curve for 										#each species.
-
+	}
 	print('Area under the curve calculated successfully')
 
 	sensitivity <- prcc_compute(species_area, sets)			#Calculate the partial rank correlation coefficient for all species
 	print('Sensitivity Calculation successful.')
 
 	sensitivity							# return prcc matrix
+	
 }
--- R/prcc_compute.r
+++ R/prcc_compute.r
@@ -7,14 +7,18 @@
 	
 	library(sensitivity)
 
-	NumSets <- nrow(sobolMatrix)
+	NumSets <- nrow(sobolMatrix)		#num of sobol sets
 	
 	nSpecies <- ncol(SpeciesMatrix)
 	
 	sens <- list()
 
-	for(i in 1:nSpecies) {
-		sens <- pcc(sobolMatrix[1:NumSets, ], SpeciesMatrix[1:NumSets, i], rank=TRUE)
+
+	for(i in 1:NumSets) {
+		#sens <- pcc(sobolMatrix[1:NumSets, ], SpeciesMatrix[1:NumSets, ], rank=TRUE)
+		sobolvect <- sobolMatrix[i,]
+		speciesvect <- SpeciesMatrix[i,]
+		sens[[i]] <- pcc(sobolvect, speciesvect, rank=TRUE)
 	}
 	sens
 }
