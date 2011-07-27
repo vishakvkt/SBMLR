86dd2f4fa421f55fb2a91f8fa6cd58771cc809c2
Vishak Venkateswaran <vishak_vkt007@yahoo.com>
2011-07-27 11:27:54 +0100

    Fixed Additional parsing bugs. Cleaned up sensitivity code.

--- R/prcc_compute.r
+++ R/prcc_compute.r
@@ -5,14 +5,19 @@
 
 "prcc_compute" <- function(SpeciesMatrix, sobolMatrix) {
 	
-	library(sensitivity)
+	library(sensitivity)			#This wont execute if the library is already loaded
 
 	NumSets <- nrow(sobolMatrix)		#num of sobol sets
 	nParams <- ncol(sobolMatrix)		#num of Parameter inputs
 	nSpecies <- ncol(SpeciesMatrix)
 	
-	sens <- matrix(data=NA, nParams, nSpecies)			# matrix param_count x Area_curve_count which is the species count
+	sens <- list()				# matrix param_count x Area_curve_count which is the species count
 
+	if(NumSets < nParams)			# If number of sobol sets less than parameter, cannot run sensitivity.
+	{
+		cat('Cannot run sensitivity when the number of sobol sets is less than the number of parameters. Simulate More points','\n')
+		return()
+	}
 	species_names <- colnames(SpeciesMatrix)
 
 	frame_spec <- data.frame(SpeciesMatrix)
@@ -20,19 +25,11 @@
 		for(j in 1:nSpecies){
 
 				senslist <- pcc(frame_sobol[1:NumSets,1:nParams],frame_spec[1:NumSets,species_names[j]], rank=TRUE)
-#				sens<-pcc(datS[1:8e4,parI:parN],datS[1:8e4,names[num]], rank = TRUE);
-				#str(senslist)
-				#prcc_l<-senslist[['PRCC']];						# extract prcc
-				#colnames(prcc_l)<-species_names[j];					# set value according to names vector containing species names
-				#str(prcc_l)								# debug for sanity
-				#print(prcc_l)
-				print(senslist)
-			}
+				prcc_l<-senslist[['PRCC']];						# extract prcc
+				sens[[i]] <- prcc_l;
+		}
 
-		#senslist <- pcc(sobolvect, speciesvect, nboot = NumSets, rank=TRUE)
-		#sens[,i] <- senslist[['PRCC']]
-		#senslist <- pcc(sobolMatrix[1:NumSets, ], speciesvect, rank=TRUE)
-	#}
-#	rownames(sens) <- colnames(sobolMatrix)
+	#colnames(sens) <- colnames(SpeciesMatrix)							# set matrix according to names vector containing species names
+	#rownames(sens) <- colnames(sobolMatrix)								# set matrix row names as the parameter names
 	sens
 }
